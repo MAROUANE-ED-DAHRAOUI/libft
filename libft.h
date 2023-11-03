c143b347b3fe344f732ae19517a2fc8340443a7c
Marouane00Dh <eddahraouimarouane@gmail.com>
2023-11-03 14:50:21 +0000

    Libft Library

--- libft.h
+++ libft.h
@@ -5,6 +5,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef struct list
+{
+	void 	*content;
+	struct list *next;
+
+}; t_list
+
 void *memcpy(void *dest, const void *src, size_t n);
 void *ft_memset(void *s, int c, size_t n);
 
