9e107a0ef7c81c576044f0c5c87cbd372a593bbb
Marouane00Dh <eddahraouimarouane@gmail.com>
2023-11-05 17:29:37 +0000

    Implement custom ft_bzero function

--- libft.h
+++ libft.h
@@ -6,18 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct list
+typedef struct s_list
 {
-	void 	*content;
-	struct list *next;
+	void *content;
+	size_t content_size;
+	struct s_list *next;
 
-} t_list;
+}	t_list;
+
 
 void *memcpy(void *dest, const void *src, size_t n);
 void * ft_memmove( void * dest, const void * src, size_t size);
 void * ft_memchr(const void *str, int c, size_t len);
 void *ft_memset(void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int ft_memcmp(const void *s1, const void *s2, size_t n);
 
 
 #endif
