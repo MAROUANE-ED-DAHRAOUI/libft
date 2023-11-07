5eb942bb6a7537c1ed0e245608fd4f6a9ac84dcf
Marouane00Dh <eddahraouimarouane@gmail.com>
2023-11-07 19:30:51 +0000

    Library :-)

--- libft.h
+++ libft.h
@@ -15,8 +15,8 @@ typedef struct s_list
 }	t_list;
 
 size_t ft_strlen(const char *s);
-void bzero(void *s, size_t n);
-void *memcpy(void *dest, const void *src, size_t n);
+void ft_bzero(void *s, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
 void * ft_memmove( void * dest, const void * src, size_t size);
 void * ft_memchr(const void *str, int c, size_t len);
 void *ft_memset(void *s, int c, size_t n);
