65e45bc1206251fad1b9106ca73044d6678d8e72
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-11-13 01:35:54 +0000

    Implement custom ft_strdup function

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,12 +3,14 @@
 char *ft_strdup(const char *s)
 {
     size_t len = ft_strlen(s);
+
     char *str = (char *)malloc(len + 1);
 
    	 if (str == NULL)
 	      return NULL;
     
         ft_memcpy(str, s, len + 1);
+
     	str[len] = '\0';
 
     	return (str);
