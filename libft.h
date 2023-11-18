40bb79b0ad73d354c9231c106c5398a59658bfde
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-11-18 19:30:28 +0000

    Library :-)

--- libft.h
+++ libft.h
@@ -30,6 +30,6 @@ void * ft_memmove( void * dest, const void * src, size_t size);
 void * ft_memchr(const void *str, int c, size_t len);
 void *ft_memset(void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-
+int ft_atoi(char *s);
 
 #endif
